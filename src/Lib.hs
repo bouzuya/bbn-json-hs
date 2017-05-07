75c373f21430fe462ae36279d498c2d3d33977b1
bouzuya <m@bouzuya.net>
2017-05-07 13:44:33 +0900

    change validJson signature

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,7 @@ module Lib
 import Control.Exception        (SomeException)
 import Control.Exception.Lifted (handle)
 import Network.HTTP.Types       (status200, status400)
-import Network.Wai              (Application, Response, responseLBS)
+import Network.Wai              (Application, Request, Response, responseLBS)
 import Network.Wai.Handler.Warp (run)
 
 runApp :: IO ()
@@ -14,13 +14,15 @@ runApp = run 3000 app
 
 app :: Application
 app request sendResponse = handle (sendResponse . invalidJson) $ do
-    sendResponse validJson
+    response <- validJson request
+    sendResponse response
 
-validJson :: Response
-validJson = responseLBS
-    status200
-    [("Content-Type", "application/json")]
-    $ "{\"message\":\"Success\"}"
+validJson :: Request -> IO Response
+validJson _ = do
+    return $ responseLBS
+        status200
+        [("Content-Type", "application/json")]
+        $ "{\"message\":\"Success\"}"
 
 invalidJson :: SomeException -> Response
 invalidJson _ = responseLBS
