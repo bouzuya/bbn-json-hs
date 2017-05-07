12e00b245cf679bd434f23dc74942d402ce6f2fc
bouzuya <m@bouzuya.net>
2017-05-07 13:29:44 +0900

    fix runApp

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( runApp
     ) where
 
+import Control.Exception        (SomeException)
+import Control.Exception.Lifted (handle)
+import Network.HTTP.Types       (status200, status400)
+import Network.Wai              (Application, Response, responseLBS)
+import Network.Wai.Handler.Warp (run)
+
 runApp :: IO ()
-runApp = putStrLn "runApp"
+runApp = run 3000 app
+
+app :: Application
+app request sendResponse = handle (sendResponse . invalidJson) $ do
+    sendResponse validJson
+
+validJson :: Response
+validJson = responseLBS
+    status200
+    [("Content-Type", "application/json")]
+    $ "{\"message\":\"Success\"}"
+
+invalidJson :: SomeException -> Response
+invalidJson _ = responseLBS
+    status400
+    [("Content-Type", "application/json")]
+    $ "{\"message\":\"Error\"}"
